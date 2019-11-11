cee9d2bd662c6b81810f8d24761c7da252888ea0
Shadrach Gabriel Penano <33576257+Shadrachp@users.noreply.github.com>
2019-11-11 17:02:29 +0800

    Added Skeleton Code from PE2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,54 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 });
 
+/*
+EXPR: TERM | EXPR + TERM | EXPR - TERM
+TERM: FACTOR | FACTOR * TERM | FACTOR / TERM
+FACTOR: TERM | EXPR
+*/
+
 var flag = true;
 var op_flag = false;
 var eq_flag = false;
+
+const Parser = () =>{
+    Parser.input = null;
+    Parser.curr=null;
+    // Parser.count = 0;
+};
+
+function init_parser (str) {
+  Parser.input = str;
+  Parser.curr = Parser.input.charAt(0);
+}
+
+Parser.next = () => {
+  Parser.input = Parser.input.slice(1,);
+  Parser.curr = Parser.input.charAt(0);
+  // Parser.count++;
+}
+Parser.EXPR = ()=>{
+  // let result = Parser.term();
+  // Parser.next();
+
+  return Parser.input;
+};
+
+Parser.TERM = () => {
+
+};
+
+Parser.FACTOR = () => {
+
+};
+
+Parser.isNum = (str) => {
+  return !isNaN(str);
+}
+
+
+
+
 
 function get_value(b) {
   b = b ||  window.event;
@@ -26,31 +71,28 @@ function get_value(b) {
       set_value('in', 0);
       set_value('res', 0);
       flag = !flag;
-    } else if (b.className === "btn uc") {
-      let v1 = get_el_value('in');
-      if(v1.length == 1)
+    } else if(b.innerHTML.trim() === 'CE') {
+        set_value('in', 0);
+        set_value('res', 0);
         flag = !flag;
-      set_value('in', v1.length == 1 ? 0 : v1.slice(0,-1));
+    } else if (b.className === "btn uc") {
+        let v1 = get_el_value('in');
+        if(v1.length == 1)
+          flag = !flag;
+        set_value('in', v1.length == 1 ? 0 : v1.slice(0,-1));
     } else if (b.className.includes("btn-operand")) {
       if(b.innerHTML.trim() !== '=')
         append_value('in', n.trim());
       else {
 
+        // alert(get_el_value('in'));
+        init_parser(get_el_value('in'));
+        alert(Parser.EXPR());
+        set_value('res', Parser.EXPR());
       }
     }
   }
 }
-
-const ParseS = () =>{
-    ParseS.st = null;
-    ParseS.curr=null;
-    ParseS.count = 0;
-};
-ParseS.next = () => {
-  ParseS.curr = charAt(ParseS.count);
-}
-ParseS();
-
 
 
 
